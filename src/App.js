ee19e15d653a8b559699c7ee78b2f3c1c452c60b
Prabhat <prabhatkumar944@gmail.com>
2022-06-03 15:30:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  
+  return (
+   <div>
+     <h2>ExpenseItem!</h2>
+     <h2>Food Rs 10</h2>
+     <h2>Petrol Rs 100</h2>
+     <h2>Movies Rs 200</h2>
+     
+   </div>
+  );
+}
+
+export default App;
