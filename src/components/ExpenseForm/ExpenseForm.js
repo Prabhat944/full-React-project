bc26a208576636ba5acfae2a87b82eb8b8810043
Prabhat <prabhatkumar944@gmail.com>
2022-06-08 21:58:02 +0530

    useState in add new  Expense

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './ExpenseForm.css';
 import Card from '../UI/Card';
 
 const ExpenseForm = ()=>{
-    
+    const [enteredTitle,setEnteredTitle] = useState('');
+    const [enteredAmount,setEnteredAmount] = useState('');
+    const [enteredDate,setEnteredDate] = useState('');
 
     const textHandler=(event)=>{
-        console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+        console.log(enteredTitle);
     }
     const amountHandler=(event)=>{
-        console.log(event.target.value);
+        setEnteredAmount(event.target.value);
+        console.log(enteredAmount);
     }
     const dateHandler=(event)=>{
-        console.log(event.target.value);
+        setEnteredDate(event.target.value);
+        console.log(enteredDate);
     }
-return <Card>
+return <Card className='new-expense'>
     <form>
-        <div>
+        <div className='new-expense__control'>
         <label>Expense Title</label>
-        <input type='text' onChange={textHandler}/>
+        <input type='text' value={enteredTitle} onChange={textHandler}/>
+        </div>
+        <div className='new-expense__control'>
         <label>Expense Amount</label>
-        <input type='number' onChange={amountHandler}/>
+        <input type='number' value={enteredAmount} onChange={amountHandler}/>
+        </div>
+        <div className='new-expense__control'>
         <label>Date</label>
-        <input type='date' onChange={dateHandler}/>
+        <input type='date' value={enteredDate} onChange={dateHandler}/>
         </div>
         <div>
             <button type='submit'>Submit</button>
