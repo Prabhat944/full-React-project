fc5ba6c79f71c79990d3be4bc3ad1b4ba9a477da
Prabhat <prabhatkumar944@gmail.com>
2022-06-09 13:35:14 +0530

    Conditional Rendering

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -14,19 +14,26 @@ setFiltered(selected);
 const filteredObject=props.items.filter(expense=>{
   return expense.date.getFullYear().toString() === filteredYear;
 })
+
+let ExpenseContent=<p>No Expense Found</p>;
+if(filteredObject.length > 0){
+  ExpenseContent= filteredObject.map(obj=>(<ExpenseItem
+    key={obj.id}
+    id={obj.id}
+    title={obj.title}
+    amount={obj.amount}
+    date={obj.date}
+
+  />))
+}
   return  (
       <Card className="expenses">
         <div>
           <ExpenseFilter filtered={filteredYear} onFiltered={filterChangeHandler}/>
         </div>
-      { filteredObject.map(obj=>(<ExpenseItem
-        key={obj.id}
-        id={obj.id}
-        title={obj.title}
-        amount={obj.amount}
-        date={obj.date}
-    
-      />))}
+        {ExpenseContent}
+        {filteredObject.length === 1 && <p>Only single Expense here. Please add more..</p>}
+      
     </Card>
     
   );
