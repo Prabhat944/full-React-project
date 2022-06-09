37f06f1fb1674c9e4f8bed936912df819e132944
Prabhat <prabhatkumar944@gmail.com>
2022-06-09 13:03:05 +0530

    use of keys and rendering

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpenseFilter from '../ExpenseFilter/ExpenseFilter';
 
 const Expenses = (props) => {  
+  const [filteredYear,setFiltered]=useState('2020');
+const filterChangeHandler=selected=>{
+setFiltered(selected);
 
-  return (
-    <Card className="expenses">
+}
+
+
+  return  (
+      <Card className="expenses">
+        <div>
+          <ExpenseFilter filtered={filteredYear} onFiltered={filterChangeHandler}/>
+        </div>
       { props.items.map(obj=>(<ExpenseItem
         key={obj.id}
         id={obj.id}
@@ -17,6 +27,7 @@ const Expenses = (props) => {
     
       />))}
     </Card>
+    
   );
 }
 
